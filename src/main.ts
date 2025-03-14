9909361c9e9371a7cad3bf68d8d81a82689e1810
takabosoft <takabosoft@gmail.com>
2025-03-15 08:30:00 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Development Build: npx webpack -w
+ * Development Server: npx live-server docs
+ * Release Build: npx webpack --mode=production
+ * URL: http://localhost:8080/
+ */
+
+$(() => {
+    console.log("OK");
+});
